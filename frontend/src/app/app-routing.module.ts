422420fe176f6d9a9b423d8657de439b085a2b41
Yaps551 <65169192+Yaps551@users.noreply.github.com>
2022-05-23 19:20:57 +0200

    Removed redundant components

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -1,19 +1,9 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
-import { AdminGuard } from "src/shared/guards/admin.guard";
-import { AuthGuard } from "src/shared/guards/auth.guard";
-import { AuthComponent } from "./auth/auth.component";
-import { CartComponent } from "./cart/cart.component";
 import { HomeComponent } from "./home/home.component";
-import { StoreManagementComponent } from "./store-management/store-management.component";
-import { StoreComponent } from "./store/store.component";
 
 const appRoutes: Routes = [
     { path: '', component: HomeComponent },
-    { path: 'store', component: StoreComponent },
-    { path: 'cart', canActivate: [AuthGuard], component: CartComponent },
-    { path: 'auth', component: AuthComponent },
-    { path: 'management', canActivate: [AuthGuard, AdminGuard] ,component: StoreManagementComponent }
   ];
 
 @NgModule({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -5,18 +5,8 @@ import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { HomeComponent } from './home/home.component';
-import { StoreComponent } from './store/store.component';
-import { CartComponent } from './cart/cart.component';
-import { ContactComponent } from './contact/contact.component';
-import { ProductsListComponent } from './store/products-list/products-list.component';
-import { ProductComponent } from './store/products-list/product/product.component';
-import { OptionsPanelComponent } from './store/options-panel/options-panel.component';
-import { AuthComponent } from './auth/auth.component';
 import { AppRoutingModule } from './app-routing.module';
 import { FormsModule } from '@angular/forms';
-import { StoreManagementComponent } from './store-management/store-management.component';
-import { ProductManagementComponent } from './store-management/product-management/product-management.component';
-import { CartItemComponent } from './cart/cart-item/cart-item.component';
 import { BroccoliAnimationComponent } from './broccoli-animation/broccoli-animation.component';
 
 @NgModule({
@@ -24,16 +14,6 @@ import { BroccoliAnimationComponent } from './broccoli-animation/broccoli-animat
     AppComponent,
     HeaderComponent,
     HomeComponent,
-    StoreComponent,
-    CartComponent,
-    ContactComponent,
-    ProductsListComponent,
-    ProductComponent,
-    OptionsPanelComponent,
-    AuthComponent,
-    StoreManagementComponent,
-    ProductManagementComponent,
-    CartItemComponent,
     BroccoliAnimationComponent,
   ],
   imports: [
