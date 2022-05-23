e7475ed95589422535600404522be27679001651
Yaps551 <65169192+Yaps551@users.noreply.github.com>
2022-05-23 18:32:59 +0200

    Initial rebuild

--- frontend/src/app/home/home.component.ts
+++ frontend/src/app/home/home.component.ts
@@ -6,10 +6,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+  slideShowLength: number = 6;
+  currentSlideIndex: number = 0;
+  slideDuration: number = 3000;
 
-  constructor() { }
+  constructor() {}
 
   ngOnInit(): void {
+    var slideShow = document.getElementsByClassName("slide");
+    
+    console.log(this.slideShowLength);
+    this.rotateImage();
   }
 
+  rotateImage() {
+    if (this.currentSlideIndex + 1 > this.slideShowLength) this.currentSlideIndex = 1;
+    else this.currentSlideIndex++;
+
+    setTimeout(this.rotateImage.bind(this), this.slideDuration);
+  }
 }
